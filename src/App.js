dc316a88dc5a9ae016c7d875bb18805e09a3f77c
Jose Luis Jr <jnrhp@hotmail.com>
2024-05-14 17:26:07 -0300

    aplicacao do car no projeto e sua folha de estilo globalmente com container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Card } from "./componentes/Card/Card";
 import { Estilos } from "./componentes/EstilosGlobais/Estilos";
 
 function App() {
   return (
     <div>
       <Estilos/>
+      <Card>
       <h1>Freeelando</h1>
+      </Card>
     </div>
   );
 }
